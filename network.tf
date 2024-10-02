83ad3a80bb3beb1098e6b98e77740da262f1111c
EC2 Default User <ec2-user@ip-172-31-0-35.ec2.internal>
2024-10-02 00:22:16 +0000

    marcha 3

---
+++ network.tf
@@ -0,0 +1,12 @@
+variable "vpc_name" {
+    type = string 
+    default = "vpc_clc12_terraform"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+
+  tags = {
+    Name = var.vpc_name
+  }
+}
